5d54342deb0d35dcfd68884a605ca11f462512ea
EdmaryDev <edithm.bautistag@gmail.com>
2021-10-14 13:37:14 -0500

    Se inflo la vista y se agrego un evento al boton desde el ActivityMainBinding

--- app/src/main/java/com/edmarydev/theory_android/MainActivity.kt
+++ app/src/main/java/com/edmarydev/theory_android/MainActivity.kt
@@ -2,10 +2,25 @@ package com.edmarydev.theory_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.edmarydev.theory_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //inflamos la vista desde el ActivityMainBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //mostramos la vista
+        setContentView(binding.root)
+
+        //accedemos al elemento para agregarle un evento
+        binding.buttonToast.setOnClickListener { toast() }
+    }
+
+    private fun toast(){
+        Toast.makeText(this, "Soy un toast!",Toast.LENGTH_LONG).show();
     }
 }
