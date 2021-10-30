fbb811a444e395e0afb363657e928151a8c5ceac
EdmaryDev <edithm.bautistag@gmail.com>
2021-10-30 00:54:55 -0500

    Se creo el binding en el activity.kt

--- app/src/main/java/com/edmarydev/theory_android/MainActivity.kt
+++ app/src/main/java/com/edmarydev/theory_android/MainActivity.kt
@@ -2,10 +2,14 @@ package com.edmarydev.theory_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.edmarydev.theory_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
