0e602557a268ddf1f283931f563f6b9de7a87413
EdmaryDev <edithm.bautistag@gmail.com>
2021-10-29 21:20:00 -0500

    Se agrego el data binding y la data class para loa quotes

---
+++ app/src/main/java/com/edmarydev/theory_android/model/QuoteModel.kt
@@ -0,0 +1,3 @@
+package com.edmarydev.theory_android.model
+
+data class QuoteModel (val quote:String, val author:String)
---
+++ app/src/main/java/com/edmarydev/theory_android/view/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.edmarydev.theory_android.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.edmarydev.theory_android.R
+import com.edmarydev.theory_android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
