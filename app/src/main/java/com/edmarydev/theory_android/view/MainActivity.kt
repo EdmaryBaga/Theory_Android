81ce05be7c45e660bddef659ee38686aa63fd785
EdmaryDev <edithm.bautistag@gmail.com>
2021-10-29 23:09:35 -0500

    Se conecto el liveData con el activity

--- app/src/main/java/com/edmarydev/theory_android/view/MainActivity.kt
+++ app/src/main/java/com/edmarydev/theory_android/view/MainActivity.kt
@@ -2,16 +2,29 @@ package com.edmarydev.theory_android.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import com.edmarydev.theory_android.R
 import com.edmarydev.theory_android.databinding.ActivityMainBinding
+import com.edmarydev.theory_android.viewmodel.QuoteViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val quoteViewModel: QuoteViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        quoteViewModel.quoteModel.observe(this, Observer { currentQuote ->
+            binding.tvQuote.text = currentQuote.quote
+            binding.tvAuthor.text = currentQuote.author
+        })
+
+        binding.viewContainer.setOnClickListener { quoteViewModel.randomQuote() }
+
     }
+
+
 }
