2c19ed63ba0efba5272f4e847645752d0d260282
EdmaryDev <edithm.bautistag@gmail.com>
2021-10-31 00:42:40 -0500

    Se agrego la llamada de retrofit en el activity

--- app/src/main/java/com/edmarydev/theory_android/MainActivity.kt
+++ app/src/main/java/com/edmarydev/theory_android/MainActivity.kt
@@ -3,6 +3,12 @@ package com.edmarydev.theory_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.edmarydev.theory_android.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +17,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    /**
+     * Para configurar nuestro retrofit usamos el metodo Builder()
+     *.addConverterFactory(GsonConverterFactory.create(), se encargara de pasar nuestro Json a DogResponse(dataClass)
+     * con la llamada a build aplicamos lo anterior
+     * */
+
+    private fun getRetrofit():Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun searchByName(query:String){
+        CoroutineScope(Dispatchers.IO).launch{
+            val call = getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
+            val puppies = call.body()
+            if(call.isSuccessful){
+                print("Obtuvimos los datos del API")
+            }else{
+                print("Hubo un error")
+            }
+        }
     }
 }
