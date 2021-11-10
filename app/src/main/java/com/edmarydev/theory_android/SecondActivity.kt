41c2d5798469c4683d57a3a4f2879074fedb8a7b
EdmaryDev <edithm.bautistag@gmail.com>
2021-11-10 15:31:52 -0600

    Se agregaron algunos elemento al drawable

---
+++ app/src/main/java/com/edmarydev/theory_android/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.edmarydev.theory_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
