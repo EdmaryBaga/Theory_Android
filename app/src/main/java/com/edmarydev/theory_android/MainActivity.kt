27506a1fcd2c1642545cb6326c8e445724c96e58
EdmaryDev <edithm.bautistag@gmail.com>
2021-11-10 23:25:10 -0600

    Se agregaron elementos al activity

--- app/src/main/java/com/edmarydev/theory_android/MainActivity.kt
+++ app/src/main/java/com/edmarydev/theory_android/MainActivity.kt
@@ -2,10 +2,27 @@ package com.edmarydev.theory_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.edmarydev.theory_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.checkBox.setOnClickListener(){
+            if(binding.checkBox.isChecked){
+                Toast.makeText(this, "Se activo!", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this, "Se desactivo!", Toast.LENGTH_LONG).show();
+            }
+
+        }
     }
+
+    fun RecuerdaUser(view: android.view.View) {}
 }
