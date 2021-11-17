3033474977d760f1c1231cfd0413a2cbc993e047
EdmaryDev <edithm.bautistag@gmail.com>
2021-11-16 20:48:09 -0600

    Se verifica si el TextToSpeetch esta avilitado en el dispositivo

--- app/src/main/java/com/edmarydev/theory_android/MainActivity.kt
+++ app/src/main/java/com/edmarydev/theory_android/MainActivity.kt
@@ -2,10 +2,37 @@ package com.edmarydev.theory_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import com.edmarydev.theory_android.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    private lateinit var binding: ActivityMainBinding
+    var tts : TextToSpeech? =null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        tts = TextToSpeech(this,this)
+
+        var message : String = binding.tvMessage.text.toString()
+
+        Log.i("message view", message)
+
     }
+
+    override fun onInit(status: Int) {
+        binding.tvMessage.clearComposingText()
+        if(status == TextToSpeech.SUCCESS){
+            binding.tvMessage.text = "Listo!"
+        }
+        else{
+            binding.tvMessage.text = "No disponible!"
+        }
+    }
+
 }
